00a72a82ff4468df7481a794f7ff60eaa2ea1585
dv-android <devang.droid@gmail.com>
2017-08-04 18:57:42 +0530

    added to Git repo

---
+++ app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
@@ -0,0 +1,32 @@
+package com.hfad.messenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage(View view){
+
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+
+        //Intent intent = new Intent(this,ReceiveMessageActivity.class);
+        //intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE,messageText);
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,messageText);
+
+        Intent choosenIntent = Intent.createChooser(intent,"Send message..");
+        startActivity(choosenIntent);
+    }
+}
